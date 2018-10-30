df4dda2a83a96f65040d219fb644891c9840920f
Eric Barboni <skygo@apache.org>
2018-10-30 12:52:14 +0000

    script to generate website

--- Jenkinsfile-site.groovy
+++ Jenkinsfile-site.groovy
@@ -18,24 +18,53 @@
  */
 
 pipeline {
-    agent {
-        //label 'git-websites'
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true], [$class: 'MessageExclusion', excludedMessage: 'Automated site publishing.*'], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'master-branch']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/apache/incubator-netbeans-mavenutils/']]])
+    agent none
+    tools { 
+        maven 'Maven 3.3.9' 
+        jdk 'jdk8' 
+    }
+    //label 'git-websites'
+    stages {
+        stage('SCM operation'){
+            agent {label 'git-websites'}
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true], [$class: 'MessageExclusion', excludedMessage: 'Automated site publishing.*'], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'master-branch']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/apache/incubator-netbeans-mavenutils/']]])
 
-        //checkout([$class: 'GitSCM', branches: [[name: '*/asf-site']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'MessageExclusion', excludedMessage: 'Automated site publishing.*'], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'asf-site-branch']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '9b041bd0-aea9-4498-a576-9eeb771411dd', url: 'https://gitbox.apache.org/repos/asf//incubator-netbeans-mavenutils/']]])
-        
-        def mvnfoldersforsite  = ['parent','nbm-shared','nb-repository-plugin',/*'nbm-maven-harness',*/ 'nbm-maven-plugin']
-        String jdkName = jenkinsEnv.jdkFromVersion('linux', '8')
-        String mvnName = jenkinsEnv.mvnFromVersion('linux', '3.5.4')
-        
-        withMaven(jdk: jdkName, maven: mvnName) {
-            for (String mvnproject in mvnfoldersforsite) {
-                dir('master-branch'+mvnproject) {
-                    sh "mvn clean build site"
+                sh 'rm -rf asf-site-branch'
+                //sh 'mkdir asf-site-branch'
+                checkout([$class: 'GitSCM', branches: [[name: '*/asf-site']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'MessageExclusion', excludedMessage: 'Automated site publishing.*'], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'asf-site-branch']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '9b041bd0-aea9-4498-a576-9eeb771411dd', url: 'https://gitbox.apache.org/repos/asf//incubator-netbeans-mavenutils/']]])
+            }
+        }
+        stage('Build Site'){ 
+            agent {label 'git-websites'}
+            steps {
+                
+                script {
+                    def mvnfoldersforsite  = ['parent','nbm-shared','nb-repository-plugin',/*'nbm-maven-harness',*/ 'nbm-maven-plugin']
+                    def BASEDIR = pwd()
+                    for (String mvnproject in mvnfoldersforsite) {
+                        dir('master-branch/'+mvnproject) {
+                            sh "mvn clean install site -Dmaven.repo.local=${BASEDIR}/.repository"
+                            sh "mv target/site ${BASEDIR}/asf-site-branch/${mvnproject}/"
+                        }
+                    }
+                }
+                
+                
+            }
+        }
+        stage('Publish Site'){ 
+            agent {label 'git-websites'}
+            steps {
+                dir('asf-site-branch') {
+                    echo 'Adding content...'
+                    sshagent (credentials: ['9b041bd0-aea9-4498-a576-9eeb771411dd']) {
+                        sh 'git add -v .'
+                        sh 'git commit -v -m "Automated site publishing by Jenkins build ${BUILD_NUMBER}'
+                        sh 'git push -v origin asf-site'
+                    }                 
                 }
             }
-          
         }
-        
     }
-}    
+}
